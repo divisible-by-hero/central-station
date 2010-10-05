556198849645cbda7fde4a24dbd04bab64b23cdc
Derek Stegelman <dstegelman@gmail.com>
2010-10-04 19:49:46 -0500

    Model Work

--- development/application/modules/defects/controllers/defects.php
+++ development/application/modules/defects/controllers/defects.php
@@ -1,23 +1,25 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of defects
  *
  * @author derek
  */
 class Defects extends Controller {
-    //put your code here
+
+
+    private function __construct(){
+        parent::Controller();
+    }
 
     public function index(){
         $this->load->view('defectListing');
     }
 
-    public function test(){
-        $this->load->view('defectListing');
+    public function view($defectID){
+
+        $this->load->model('defect_mdl');
+        $defectData = $this->Defect_mdl->getDefect($defectID);
+        
     }
 }
 ?>
