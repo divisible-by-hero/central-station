b82ce7db60f746832508c093c8cf621cc2fca626
Derek Stegelman <dstegelman@gmail.com>
2010-10-05 11:35:13 -0500

    worked on settings model

--- development/application/modules/common/models/setting_mdl.php
+++ development/application/modules/common/models/setting_mdl.php
@@ -1,10 +1,33 @@
 <?php
 /**
- * Description of setting_mdl
+ * Gets and Sets settings for the app.
  *
- * @author derek
+ * @author Derek Stegelman
  */
 class Setting_mdl extends CI_Model {
-    
+
+    public $settingTable;
+    public $settingID;
+    public $settingName;
+    public $settingValue;
+
+    /**
+     *  Set Variables
+     *
+     * @author Derek Stegelman
+     * @updated Oct 5 2010
+     *
+     */
+
+    public function setSetting($settingName, $settingValue){
+
+        $settingData = array('settingValue'=>$settingValue);
+        $settingWhere = "settingName = '$settingName'";
+        $settingUpdate = $this->db->update_string($this->settingTable, $settingData, $settingWhere);
+        $this->db->query($settingUpdate);
+
+        
+    }
+
 }
 ?>
