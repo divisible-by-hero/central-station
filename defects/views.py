b1691600376112c7f3a03e43e948f8b776961e33
Derek Stegelman <dstegelman@gmail.com>
2011-06-18 22:18:15 -0500

    More changes

--- defects/views.py
+++ defects/views.py
@@ -4,9 +4,14 @@ from django.views.generic.simple import direct_to_template
 
 
 def defect_list(request, project_slug):
+    context = { }
+    defects = Defect.objects.filter(project__slug=project_slug)
+    context['defects'] = defects
     
     return direct_to_template(request, template="defects/defect_list.html", extra_context=context)
 
 def defect_detail(request, defect_id):
-    
+    context = { }
+    defect = Defect.objects.get(pk=defect_id)
+    context['defect'] = defect
     return direct_to_template(request, template="defects/defect_detail.html", extra_context=context)
