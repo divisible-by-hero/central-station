306659db3ac9187ca1ec7ff8e71666b0271ffb1f
Derek Stegelman <dstegelman@gmail.com>
2011-06-18 21:45:56 -0500

    defect api

--- urls.py
+++ urls.py
@@ -7,11 +7,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'myproject.views.home', name='home'),
-    # url(r'^myproject/', include('myproject.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    url(r'^defects/', include('defects.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
