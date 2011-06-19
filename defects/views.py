3db590a750da934aca6cc1409a3ba73ec604a34f
Derek Stegelman <dstegelman@gmail.com>
2011-06-18 22:03:53 -0500

    Projects added.  Defect stuff Fixed

--- defects/views.py
+++ defects/views.py
@@ -1,1 +1,12 @@
 # Create your views here.
+from defects.models import *
+from django.views.generic.simple import direct_to_template
+
+
+def defect_list(request, project_slug):
+    
+    return direct_to_template(request, template="defects/defect_list.html", extra_context=context)
+
+def defect_detail(request, defect_id):
+    
+    return direct_to_template(request, template="defects/defect_detail.html", extra_context=context)
