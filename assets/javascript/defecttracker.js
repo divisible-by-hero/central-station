5aa3b8f2913e4ae4c10064e9cc855568a7c2a324
Derek Stegelman <dstegelman@gmail.com>
2011-06-18 22:36:32 -0500

    more

--- assets/javascript/defecttracker.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($) {
-      $('a[rel*=facebox]').facebox({
-        loading_image : '../facebox/loading.gif',
-        close_image   : '../facebox/closelabel.gif'});
-});
-
-
