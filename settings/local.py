e8eacb031a569ce3328bc97a4af7b9af98ef24bb
Derek Stegelman <dstegelman@gmail.com>
2011-08-12 20:38:59 -0500

    Adding proifle

--- settings/local.py
+++ settings/local.py
@@ -112,6 +112,8 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'urls.local'
 
+AUTH_PROFILE_MODULE = 'profile.UserProfile'
+
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
@@ -139,6 +141,7 @@ INSTALLED_APPS = (
     'newsfeed',
     'django.contrib.admin',
     'south',
+    'profile',
 
 )
 
--- urls/local.py
+++ urls/local.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     url(r'^defects/', include('defects.urls')),
     url(r'^projects/', include('projects.urls')),
     url(r'^browser/', include('browser.urls')),
+    url(r'^users/', include('profile.urls')),
     
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'profile/login.html'}),
     url(r'^admin/', include(admin.site.urls)),
