33e69b673f79fc39c0640c0559a6adc938e09be3
Derek Stegelman <dstegelman@gmail.com>
2011-08-19 21:44:13 -0500

    Ommit the users from the settings form of an app

--- apps/projects/forms.py
+++ apps/projects/forms.py
@@ -4,6 +4,7 @@ from projects.models import *
 class ApplicationForm(forms.ModelForm):
     class Meta:
         model = App
+        exclude = ('users',)
         
 class VersionForm(forms.ModelForm):
     class Meta:
