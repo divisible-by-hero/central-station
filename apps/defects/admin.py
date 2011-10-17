4af08b75e5dd8b9e2bab8cf861ad779ffcd12aae
Derek Stegelman <dstegelman@gmail.com>
2011-10-17 13:59:23 -0500

    Remove blog and defects app

--- apps/defects/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from defects.models import *
-
-class DefectAdmin(admin.ModelAdmin):
-    save_on_top = True
-    list_display = ('description', 'type', 'status', 'application')
-    
-admin.site.register(Defect, DefectAdmin)
-
