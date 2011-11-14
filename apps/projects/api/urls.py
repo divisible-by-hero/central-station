5ca0aac4745d215df0e6a8e2c72cc9f5096f72a2
Derek Stegelman <dstegelman@gmail.com>
2011-11-14 15:41:47 -0600

    Rmeove piston

--- apps/projects/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-from projects.api.handlers.app import AppsHandler
-from api_manager.utils import CsrfExemptResource
-
-apps_handler = CsrfExemptResource(AppsHandler)
-urlpatterns = patterns('',
-	url(r'^$', apps_handler),
-)
