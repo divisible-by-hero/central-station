dfe5ed20155c402d8cdd1798e69864f36fb9a8eb
unknown <Derek@Derek-PC.(none)>
2011-11-14 22:15:55 -0600

    fab change

--- conf/fabfile.py
+++ conf/fabfile.py
@@ -15,34 +15,39 @@ env.static_dir = '~/static/prod'
 env.project_virtual = '~/.virtualenvs/%s' % PROJECT_ID
 env.activate = 'source ~/.virtualenvs/%s/bin/activate' % PROJECT_ID
 env.deploy_user = PROJECT_USER
+env.apache_bin_dir = "~/webapps/myproj/apache2/bin"
+env.log_location = "~/webapps/logs/user/error_myproj.log"
+env.git_repo = "git@github.com:myuser/%s.git" % PROJECT_ID
+env.production_branch = "production"
+
+# Utility Methods
+def view_log():
+    run('sudo cat %s' % env.log_location)
 
 def kick_apache():
-    with cd('~/apache2/bin'):
+    with cd(env.apache_bin_dir):
         run("./restart")
 
 def virtualenv(command):
     with cd(env.directory):
         run(env.activate + '&&' + command)
 
-def pip_install_req(env):
-    if env == 'local':
-        local("pip install -r conf/requirements.txt")
-    else:
-        virtualenv('pip install -r conf/requirements.txt') 
+def install_requirements():
+    virtualenv('pip install -r conf/requirements.txt')
 
 def get_code():
     with cd(env.directory):
         run('git pull origin production')
         
 def copy_static():
-    with cd(env.directory + 'assets'):
+    with cd(env.directory + '/static'):
         run('cp -r * ' + env.static_dir)
-
+        
 def sync_db(env):
     if env == "local":
         local("python manage.py syncdb --settings=settings.local")
     else:
-        virtualenv('python manage.py syncdb --settings=settings.production')
+        virtualenv('python manage.py syncdb --settings=settings.%s' % env)
     
 def migrate(env):
     if env == "local":
@@ -64,50 +69,24 @@ def build_docs():
     local('cd docs && make html')
     
 
-def setup():
-    local("clear")
-    print("Running Setup Script..I think..")
-    
-    #run("mkdir " + env.directory)
-    #with cd(env.virtual_dir):
-    #    run('virtualenv %s --no-site-packages' % PROJECT_ID)
-    #run("git clone " + env.git_repo + " " + env.directory)
-    #with cd(env.directory):
-    #    run('git checkout production')
-    virtualenv("easy_install http://downloads.sourceforge.net/project/mysql-python/mysql-python-test/1.2.3c1/MySQL-python-1.2.3c1.tar.gz?use_mirror=voxel")
-    deploy()
+
 
 #Local Commands
 def run_local():
     local('python manage.py runserver --settings=settings.local')
   
-#Utils    
-def samuel_l_jackson():
-    local('clear')
-    print('access main program')
-    time.sleep(1)
-    print('access main security')
-    time.sleep(1)    
-    print('access main program grid')
-    time.sleep(3)    
-    print('')
-    print('')
-    print('Hold on to yer butts...')
-    print('')
-    print('')
-    
-    
+   
 
 def deploy():
-    local("clear")
-    #samuel_l_jackson()
-    pip_install_req()
-    get_code()
-    #copy_static()
+    install_requirements()
+    pull()
+    copy_static()
     sync_db("production")
-    migrate("production")
-    kick_apache()
-    print("Deployment completed.")
+    migration("production")
+    # Either touch or restart of apache.
+    local('touch ~/projects/%s/conf/%s.wsgi' % env.id)
+    #kick_apache()
+    print('Deployment of %s complete' % env.id)
 
 def run_local_server():
     build_docs()
