895fc60bbb8e8552fe67bf96681ccb38f169b550
Derek Stegelman <dstegelman@gmail.com>
2012-03-01 18:58:49 -0600

    remove others.

--- apps/chats/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import patterns, include, url
-from chats.views import *
-
-urlpatterns = patterns('',
-    url(r'^$', rooms),
-    url(r'^room/(?P<room_slug>[-\w]+)/$', room, name="room"),
-    #url(r'^app/(?P<app_slug>[-\w]+)/settings/$', settings, name="app"),
-    
-)
