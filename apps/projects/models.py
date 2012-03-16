34c71f18f861952417bda582355c2dee16c681eb
Derek Stegelman <dstegelman@gmail.com>
2012-03-15 20:46:53 -0500

    fixes

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -8,7 +8,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from hadrian.utils.slugs import unique_slugify
 from projects.choices import *
-
+from issues.models import *
 
 class App(models.Model):
     name = models.CharField(max_length=250)
@@ -26,9 +26,10 @@ class App(models.Model):
     @models.permalink
     def get_absolute_url(self):
         return ('projects.views.app', (), {'app_slug': self.slug})
-        
+    
+    @property        
     def open_defects(self):
-        return "6"
+        return Issues.objects.open().by_app(self.slug).count()
     
     
 
