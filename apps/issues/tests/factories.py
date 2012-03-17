e2a56c411d7cb5927fbd673a464331d54d0786e0
Derek Stegelman <dstegelman@gmail.com>
2012-03-16 22:42:28 -0500

    Stubbing

--- apps/issues/tests/factories.py
+++ apps/issues/tests/factories.py
@@ -1,5 +1,11 @@
 import factory
 from issues.models import Issue
+from django.contrib.auth.models import User
+
+class UserFactory(factory.Factory):
+    FACTORY_FOR = User
+
+    username = "test_user"
 
 class IssueFactory(factory.Factory):
     FACTORY_FOR = Issue
