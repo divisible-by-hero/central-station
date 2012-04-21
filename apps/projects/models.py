a4cdd35c28a2adecc043748e56f68e8017909c27
Derek Stegelman <dstegelman@gmail.com>
2012-04-21 11:39:37 -0500

    Model work

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -1,22 +1,21 @@
-'''
-    Author: Derek Stegelman
-    Package: Projects App
-
-'''
-
 from django.db import models
 from django.contrib.auth.models import User
 from hadrian.utils.slugs import unique_slugify
-from projects.choices import *
-from issues.models import *
 
 class App(models.Model):
     name = models.CharField(max_length=250)
     slug = models.SlugField(editable=False)
+    date_started = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
+    date_due = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
+    project_manager = models.ForeignKey(User, blank=True, null=True)
+    client = models.CharField(max_length=250, blank=True, null=True) #This might be a foreig key
+    site_url = models.URLField(max_length=500, blank=True, null=True)
+    enable_chats = models.BooleanField(default=False)
+    enable_repo = models.BooleanField(default=False)
     enable_issues = models.BooleanField(default=False)
-    users = models.ManyToManyField(User)
+    developers = models.ManyToManyField(User, related_name="app_developers", blank=True, null=True)
     
-    def __unicode__(self):
+    def __unicode__(self): # pragma: no cover
         return self.name
     
     def save(self, *args, **kwargs):
