cc0c1ea95c61d701b817cac573ded568ac220b7a
Derek Stegelman <dstegelman@gmail.com>
2012-08-04 13:34:03 -0500

    stack upgrades

--- settings/common.py
+++ settings/common.py
@@ -80,12 +80,12 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 ) 
 
 MIDDLEWARE_CLASSES = (
+    'downtime.middleware.DowntimeMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    
 )
 
 
