fde83d3eb745363a61bafc32cbec6642dde3f462
Derek Stegelman <dstegelman@gmail.com>
2012-09-06 15:38:37 -0400

    Adding list view.

--- apps/sprints/urls.py
+++ apps/sprints/urls.py
@@ -7,10 +7,15 @@ from django.conf.urls import patterns, url, include
 from tastypie.api import Api
 
 from sprints.api import SprintResource
+from sprints.views import SprintListView
 
 v1_api = Api(api_name="v1")
 v1_api.register(SprintResource())
 
 urlpatterns = patterns('',
+
+    url(r'^$', SprintListView.as_view(), name='sprint_list_view'),
+
+    # API
     url(r'^api/', include(v1_api.urls)),
 )
