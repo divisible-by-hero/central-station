d57027c11c45ea5fa5d6cc649e45a503b37ea47f
Derek Stegelman <dstegelman@gmail.com>
2012-10-19 11:19:45 -0500

    feature accounts

--- apps/accounts/context_processor.py
+++ apps/accounts/context_processor.py
@@ -2,8 +2,11 @@ __author__ = 'Derek Stegelman'
 __date__ = '10/19/12'
 
 from accounts.models import RoleAssigned
+from sprints.models import Sprint
 
 def accounts(request):
     teams = RoleAssigned.objects.filter(user=request.user)
     context = {'teams': teams, 'account_slug': request.session.get('account_slug')}
+    account_sprints = Sprint.objects.current().filter(team__organization__slug=request.session.get('account_slug'))
+    context['account_sprints'] = account_sprints
     return context
