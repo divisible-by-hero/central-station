cb0c0e24047f049a15e88883b50fdd1e75cd53ea
Derek Stegelman <dstegelman@gmail.com>
2012-12-03 19:09:34 -0600

    Add additional admins.

--- apps/sprints/admin.py
+++ apps/sprints/admin.py
@@ -2,7 +2,7 @@ __author__ = 'Derek Stegelman'
 __date__ = '9/6/12'
 from django.contrib import admin
 
-from sprints.models import Story, Roadblock, Sprint, Task, StoryStatus
+from sprints.models import Story, Roadblock, Sprint, Task, StoryStatus, SprintStory
 
 class StoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'story_status', 'sprint')
@@ -11,8 +11,12 @@ class StoryAdmin(admin.ModelAdmin):
 class StoryStatusAdmin(admin.ModelAdmin):
     list_display = ('status', 'account')
 
+class SprintAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_date', 'end_date')
+
 admin.site.register(Story, StoryAdmin)
 admin.site.register(StoryStatus, StoryStatusAdmin)
 admin.site.register(Roadblock)
-admin.site.register(Sprint)
+admin.site.register(Sprint, SprintAdmin)
 admin.site.register(Task)
+admin.site.register(SprintStory)
