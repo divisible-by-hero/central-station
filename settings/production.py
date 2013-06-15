2756c531474733d1702a5e70f75dcdf2ca9667bf
Derek Stegelman <dstegelman@gmail.com>
2013-06-15 06:22:36 -0600

    Remove dead password.

--- settings/production.py
+++ settings/production.py
@@ -10,9 +10,9 @@ THUMBNAIL_DEBUG = DEBUG
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'dstegelman_cen',                      # Or path to database file if using sqlite3.
-        'USER': 'dstegelman_cen',                      # Not used with sqlite3.
-        'PASSWORD': 'uYK4aAHJ',                  # Not used with sqlite3.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
